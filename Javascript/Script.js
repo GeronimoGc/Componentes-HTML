4fb88828301e3bb18a1eb314291a6f57dc87d232
GeronimoGc <103221061+GeronimoGc@users.noreply.github.com>
2024-01-01 18:37:47 -0500

    Componets Scripts

---
+++ Javascript/Script.js
@@ -0,0 +1,79 @@
+// Obtener la barra de navegación
+// var Navbar = document.querySelector('.navbar-togglable')
+// var NavbarButton = document.querySelector('.navbar-toggler')
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     // Obtener el botón
+//     var NavbarButton = document.getElementById('Navbar-screen-button')
+
+//     // Verificar el valor actual de aria-expanded
+//     var AriaExpanded = NavbarButton.getAttribute('aria-expanded')
+
+//     // Condición para cambiar el valor de aria-expanded
+//     if (AriaExpanded === 'false') {
+//         window.addEventListener('scroll', function () {
+//             // Verificar si la ventana se ha desplazado más allá de la parte superior de la barra de navegación
+//             if (window.scrollY > Navbar.offsetTop) {
+//                 // Si es así, agregar la clase "navbar-light" y eliminar la clase "navbar-dark"
+//                 Navbar.classList.add('navbar-light')
+//                 Navbar.classList.remove('navbar-dark')
+//             } else {
+//                 // De lo contrario, agregar la clase "navbar-dark" y eliminar la clase "navbar-light"
+//                 Navbar.classList.add('navbar-dark')
+//                 Navbar.classList.remove('navbar-light')
+//             }
+//         })
+//     } else {
+//         document
+//             .querySelector('.navbar-toggler')
+//             .addEventListener('click', function () {
+//                 // Verificar si la barra de navegación es de color oscuro
+//                 if (Navbar.classList.contains('navbar-dark')) {
+//                     // Si es así, cambiar la clase a "navbar-light"
+//                     Navbar.classList.remove('navbar-dark')
+//                     Navbar.classList.add('navbar-light')
+//                 } else {
+//                     // De lo contrario, cambiar la clase a "navbar-dark"
+//                     Navbar.classList.remove('navbar-light')
+//                     Navbar.classList.add('navbar-dark')
+//                 }
+//             })
+
+
+//     }
+// })
+
+// document.addEventListener('DOMContentLoaded', function () {
+//     // Agregar un evento de desplazamiento a la ventana
+// if(NavbarButton. ){
+//     window.addEventListener('scroll', function () {
+//         // Verificar si la ventana se ha desplazado más allá de la parte superior de la barra de navegación
+//         if (window.scrollY > Navbar.offsetTop) {
+//             // Si es así, agregar la clase "navbar-light" y eliminar la clase "navbar-dark"
+//             Navbar.classList.add('navbar-light')
+//             Navbar.classList.remove('navbar-dark')
+//         } else {
+//             // De lo contrario, agregar la clase "navbar-dark" y eliminar la clase "navbar-light"
+//             Navbar.classList.add('navbar-dark')
+//             Navbar.classList.remove('navbar-light')
+//         }
+//     })
+//     // Agregar un evento de clic al botón de alternancia de navegación
+
+// } else {
+//     document
+//     .querySelector('.navbar-toggler')
+//     .addEventListener('click', function () {
+//         // Verificar si la barra de navegación es de color oscuro
+//         if (Navbar.classList.contains('navbar-dark')) {
+//             // Si es así, cambiar la clase a "navbar-light"
+//             Navbar.classList.remove('navbar-dark')
+//             Navbar.classList.add('navbar-light')
+//         } else {
+//             // De lo contrario, cambiar la clase a "navbar-dark"
+//             Navbar.classList.remove('navbar-light')
+//             Navbar.classList.add('navbar-dark')
+//         }
+//     })
+// }
+// })
